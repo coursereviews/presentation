056c8b3d8e1154c5bbd70155dd365eee73331355
Dana Silver <dsilver1221@gmail.com>
2014-09-05 17:57:29 -0400

    Added slides: pres. link, basics, quota, help, comments.

--- presentation.js
+++ presentation.js
@@ -11,7 +11,7 @@ d3.json('analytics.json', function (stats) {
   stats.reviews_by_day.splice(0, 5);
 
   lineChart('#users', stats.users_by_day, 'orange');
-  lineChart('#reviews', stats.reviews_by_day, 'steelblue');
+  lineChart('#reviews', stats.reviews_by_day, 'lightskyblue');
 });
 
 function lineChart (section, stats, color) {
@@ -84,11 +84,83 @@ function lineChart (section, stats, color) {
 }
 
 d3.json('quota_stats.json', function (stats) {
-  var margin = {top: 20, right: 20, bottom: 50, left: $('#quota').width() / 20},
+  var margin = {top: 20, right: 20, bottom: 70, left: $('#quota').width() / 20 + 20},
       height = $('#quota').height() - $('#quota h1').outerHeight(true) - margin.top - margin.bottom,
       width = $('#quota').width() - margin.left - margin.right;
 
-  console.log(stats);
+  var x = d3.scale.ordinal()
+      .domain(stats.map(function (d) { return d.reviews; }))
+      .rangeRoundBands([0, width], .1);
+
+  var y = d3.scale.linear()
+      .domain([0, d3.max(stats, function (d) { return d.users; })])
+      .range([height, 0]);
+
+  var xAxis = d3.svg.axis()
+      .scale(x)
+      .orient('bottom');
+
+  var yAxis = d3.svg.axis()
+      .scale(y)
+      .orient('left');
+
+  var svg = d3.select('#quota svg')
+      .attr('width', width + margin.left + margin.right)
+      .attr('height', height + margin.top + margin.bottom)
+    .append('g')
+      .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+  svg.append('g')
+      .attr('class', 'y axis')
+      .call(yAxis)
+    .append('text')
+      .attr('x', -height / 2)
+      .attr('y', '-3em')
+      .attr('transform', 'rotate(-90)')
+      .style('text-anchor', 'middle')
+      .text('users');
+
+  svg.append('g')
+      .attr('class', 'x axis')
+      .attr('transform', 'translate(0,' + height + ')')
+      .call(xAxis)
+    .append('text')
+      .attr('x', width / 2)
+      .attr('y', '2.5em')
+      .style('text-anchor', 'middle')
+      .text('reviews');
+
+  var bar = svg.selectAll('.bar')
+      .data(stats)
+    .enter().append('g')
+      .attr('class', 'bar')
+      .attr('transform', function (d) { return 'translate(' + x(d.reviews) + ',0)'; });
+
+  bar.append('rect')
+      .attr('y', function (d) { return y(d.users); })
+      .attr('height', function (d) { return height - y(d.users); })
+      .attr('width', x.rangeBand())
+      .style('fill', function (d) {
+        if (d.reviews > 2) return 'orange';
+        return 'lightskyblue';
+      });
+
+  bar.append('text')
+      .attr('x', x.rangeBand() / 2)
+      .attr('y', function (d) { return y(d.users); })
+      .attr('dy', function (d) {
+        if (d.reviews === 0 || d.reviews === 2) return '1.2em';
+        return '-.6em';
+      })
+      .style('text-anchor', 'middle')
+      .style('font-size', '.7em')
+      .style('fill', function (d) {
+        if (d.reviews === 0 || d.reviews === 2) return '#000';
+        return '#fff';
+      })
+      .text(function (d) {
+        return d.users;
+      });
 });
 
 d3.json('courses_code_stats.json', function (stats) {
@@ -149,7 +221,7 @@ d3.json('courses_code_stats.json', function (stats) {
       .attr('y', function (d) { return y(d.count); })
       .attr('height', function (d) { return height - y(d.count); })
       .attr('width', x.rangeBand())
-      .style('fill', 'steelblue');
+      .style('fill', 'lightskyblue');
 
   bar.append('text')
       .attr('x', x.rangeBand() / 2)
@@ -160,7 +232,10 @@ d3.json('courses_code_stats.json', function (stats) {
       })
       .style('text-anchor', 'middle')
       .style('font-size', '.7em')
-      .style('fill', '#fff')
+      .style('fill', function (d) {
+        if (d.level < 4) return '#000';
+        return '#fff';
+      })
       .text(function (d) {
         if (d.count === 1) return d.count + ' review';
         return d.count + ' reviews';
